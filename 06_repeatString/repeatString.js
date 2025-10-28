5c675c6acaf3d1545e896a9b7a9b22ce14092029
Patrick Rogers <225264952+codewithprogers@users.noreply.github.com>
2025-10-27 20:22:28 -0700

    feat: add repeatString function with loop
    Created a functions that repeats a string num times and returns ERROR if
    num is negative. Instead of using the .repeat() method I used utilized a
    for loop instead.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if(num < 0) return "ERROR";
+  let res = "";
+  for (let i = 0; i < num ; i++) {
+    const number = Math.floor(Math.random() * 999);
+    res += string;
+  }
+  return res;
 };
+
+repeatString('hey', 3);
+repeatString('hello', 10);
+repeatString('hi', 1);
+repeatString('bye', 0);
+repeatString('goodbye', -1);
+repeatString('', 10);
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
