3501890b4d43a2e0384f70d9de42fd1b6299c20a
Patrick Rogers <225264952+codewithprogers@users.noreply.github.com>
2025-10-27 21:32:23 -0700

    feat: add reverseString function
    Created the reverseString function which takes in a string, converts it
    to an array, reverses that array to be backwards, then joins the array
    back into a string.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+// const reverseString = function(word) {
+//   let charArray = word.split('');
+//   charArray.reverse();
+//   return charArray.join('');
+// };
 
-};
+const reversString = function(string) {
+  return string.split("").reverse().join("");
+}
+
+reverseString('hello');
+reverseString('hello there');
+reverseString('123! abc! Hello, Odinite.');
+reverseString('');
 
 // Do not edit below this line
 module.exports = reverseString;
