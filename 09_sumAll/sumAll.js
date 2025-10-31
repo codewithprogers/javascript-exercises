cc98986d101e70f8d4f9ec64472e7ba3bb25ef45
Patrick Rogers <225264952+codewithprogers@users.noreply.github.com>
2025-10-31 01:24:36 -0700

    feat: ass sumAll function
    Implemented the sumAll function which checks if either input is
    negative, not a number, or not an integer and if either is, gives an
    error. If everything checks out, the functions takes all the numbers
    from the smaller to the larger input, adds them together, and returns
    the total.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(numA, numB) {
+  let sum = 0;
+  if(Math.min(numA, numB) < 0 || typeof numA !== "number" || typeof numB !== "number" || !Number.isInteger(numA) ||!Number.isInteger(numB)) return "ERROR";
+  for(let i = Math.min(numA, numB); i <= Math.max(numA, numB); i++) {
+    sum = sum + i;
+  }
+  return sum;
 };
+
+sumAll(2, 4);
+sumAll(1, 4000);
+sumAll(123, 1);
+sumAll(-10, 4);
+sumAll(2.5, 4);
+sumAll(10, "90");
+sumAll(10, [90, 1]);
 
 // Do not edit below this line
 module.exports = sumAll;
