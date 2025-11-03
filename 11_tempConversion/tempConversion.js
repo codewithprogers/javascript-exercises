ed0b1780771db9592fce75ad46435fd16af49d59
Patrick Rogers <225264952+codewithprogers@users.noreply.github.com>
2025-11-02 19:18:53 -0800

    feat: implement temperature conversion functions
    Added convertToCelsius and convertToFahrenheit functions.
    convertToCelsius converts Fahrenheit to Celsius and rounds to one
    decimal place. convertToFahrenheit converts Celsius to Fahrenheit
    and also rounds to one decimal place.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeF) {
+  let celsius = 5 / 9 * (degreeF - 32);
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeC) {
+  let fahrenheit = degreeC * 9 / 5 + 32;
+  return Math.round(fahrenheit * 10) / 10;
 };
+
+convertToCelsius(32);
+convertToCelsius(100);
+convertToCelsius(-100);
+
+convertToFahrenheit(0);
+convertToCelsius(73.2);
+convertToCelsius(-10);
 
 // Do not edit below this line
 module.exports = {
