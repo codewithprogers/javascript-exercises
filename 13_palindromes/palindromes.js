b003cf331c4b3248cbcb6203d2034c5bf2008069
Patrick Rogers <225264952+codewithprogers@users.noreply.github.com>
2025-12-13 23:44:49 -0800

    feat: add palindromes function
    This function first set the inputted word to lowercase, checks to see if
    each character is either a letter a-z or a number 0-9, then stores those
    chosen characters into a variable declared as cleaned. Once the string
    is completely checked it is then converted into an array, having its
    order reversed, then converted back into a string. The original cleaned
    string is then compared to the reversed string to see if it is a
    palindrome. True if yes, false if no.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  const lower = word.toLowerCase();
+  let cleaned = "";
+  for (let i = 0; i < lower.length; i++) {
+    let char = lower[i];
+    if ("a" <= char && char <= "z" || "0" <= char && char <= "9") {
+      cleaned = cleaned + char
+    };
+  };
+  let reversed = [...cleaned].reverse().join("");
+  return cleaned === reversed; 
 };
+
+console.log(palindromes('racecar'));
+console.log(palindromes('racecar!'));
+console.log(palindromes('Racecar!'));
+console.log(palindromes('A car, a man, a maraca.'));
+console.log(palindromes('Animal loots foliated detail of stool lamina.'));
+console.log(palindromes('ZZZZ car, a man, a maracaz.'));
+console.log(palindromes('rac3e3car'));
+console.log(palindromes('r3ace3car'));
 
 // Do not edit below this line
 module.exports = palindromes;
