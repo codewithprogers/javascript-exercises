dbbb1d07a3789fda48aed8bf475dc345c7f76e4d
Patrick Rogers <225264952+codewithprogers@users.noreply.github.com>
2025-12-16 00:48:11 -0800

    feat: add fibonacci function
    Handles negative numbers by returning OOPS and returns 0 for input of 0.
    Accepts string inputs by converting them to numbers.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,35 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+  let first = 1;
+  let second = 1;
 
+  number = Number(number);
+
+  if (number < 0 ) {
+    return "OOPS";
+  } else if (number === 0) {
+    return 0;
+  };
+
+  for (let i = 2; i < number; i++) {
+    let next = first + second;
+    first = second;
+    second = next;
+  };
+
+  return second;
 };
+
+fibonacci(4);
+fibonacci(6);
+fibonacci(10);
+fibonacci(15);
+fibonacci(25);
+fibonacci(0);
+fibonacci(-25);
+fibonacci("0");
+fibonacci("1");
+fibonacci("2");
+fibonacci("8");
 
 // Do not edit below this line
 module.exports = fibonacci;
