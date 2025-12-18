afbbe0ff3363023056ff549f752edc188f9ca658
Patrick Rogers <225264952+codewithprogers@users.noreply.github.com>
2025-12-17 21:57:18 -0800

    feat: add findTheOldest function
    Subtracts the year of death by the birth year to get the person's age,
    then compares them, returning whomever is the oldest.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,37 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 2018,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
 
+const findTheOldest = function(arr) {
+  return arr.reduce((first, current) => {
+    let oldestAge = first.yearOfDeath - first.yearOfBirth;
+    let currentAge = current.yearOfDeath - current.yearOfBirth;
+    
+    if (currentAge > oldestAge) {
+      return current;
+    } else {
+      return first;
+    }
+  }, arr[0]);
 };
+
+console.log(findTheOldest(people).name);
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
